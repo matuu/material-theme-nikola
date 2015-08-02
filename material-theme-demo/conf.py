354114770e6e865fac1865ad47ad1b3b235d8afa
Matías Varela <matu.varela@gmail.com>
2015-08-02 09:49:21 -0300

    more minors changes

--- material-theme-demo/conf.py
+++ material-theme-demo/conf.py
@@ -946,7 +946,7 @@ UNSLUGIFY_TITLES = True
 # }
 
 # If webassets is installed, bundle JS and CSS to make site loading faster
-USE_BUNDLES = False
+USE_BUNDLES = True
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
